f3323c2d3bb169832350051db1d0674abf584b45
Floris Padt <floris@fmpadt.nl>
2024-11-29 22:04:25 +0100

    new laptop 21-24

--- notebooks/10_data_import.R
+++ notebooks/10_data_import.R
@@ -1,1 +1,29 @@
-# data Access
+# Data Access
+
+# Paths to Parquet Files
+
+# Define the path to your Parquet file
+
+PARR <- file.path(PS02, SYS, "RTP", "ARR")
+
+# Material 
+FN_MATL <- 
+  file.path(PARR, "MD_MATERIAL.parquet") %>% 
+  normalizePath() 
+
+FN_MATS <- 
+  file.path(PARR, "MD_MATERIAL_SALES_ORG.parquet") %>% 
+  normalizePath() 
+
+FN_MATP <- 
+  file.path(PARR, "MD_MATERIAL_PLANT.parquet") %>% 
+  normalizePath() 
+
+FN_MATP <- 
+  file.path(PARR, "MD_SOLD_TO_CUSTOMER.parquet") %>% 
+  normalizePath() 
+
+# Sales
+FN_ISLS <- 
+  file.path(PARR, "DD_HISTO_QTY_21-24.parquet") %>% 
+  normalizePath() 
