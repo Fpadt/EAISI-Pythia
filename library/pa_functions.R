cf1f3dd3008b1f6b91d9746bf0a49ca973acd3b9
Floris Padt <floris@fmpadt.nl>
2024-12-15 22:54:43 +0100

    tsfeatures

--- library/pa_functions.R
+++ library/pa_functions.R
@@ -1,50 +1,4 @@
-fGet_Sales_by_Material <- 
-  function(material, salesorg){
-    
-    # Establish a connection to DuckDB
-    con <- dbConnect(duckdb(), dbdir = ":memory:")
-    
-    MATL <- c(MATN1(material))
-    SORG <- c(salesorg)
-    TABL <- FN_ISLS
-    
-    query <-   
-      glue_sql("
-    SELECT 
-      SALESORG,
-      MATERIAL,
-      CALMONTH,
-      sum(SLS_QT_SO + SLS_QT_FOC) as DEMND_QTY
-    FROM 
-      read_parquet({`TABL`})
-    WHERE
-      MATERIAL IN ({MATL*}) AND
-      SALESORG in ({SORG*}) AND
-      CALMONTH < date_trunc('month', current_date)
-    GROUP BY 
-      ALL
-    ORDER BY 
-      MATERIAL,
-      CALMONTH
-    ", .con = con
-      )
-    
-    # return data.table
-    dtTS <- 
-      dbGetQuery(con, query, n = Inf) %>%
-      setDT() %>%
-      .[, .(
-        unique_id = paste(SALESORG, MATERIAL, sep = "_"),
-        ds        = CALMONTH,
-        y         = DEMND_QTY
-      )
-      ]
-    
-    # Disconnect from DuckDB
-    dbDisconnect(con)
-    
-    return(dtTS)
-  }
+# Master data Functions ####
 
 fGet_MATL <- 
   function(){
@@ -127,6 +81,56 @@ fGet_MATP <-
     dbDisconnect(con)
     
     return(MATP)
+  }
+
+# Sales Functions ####
+
+fGet_Sales_by_Material_Salesorg <- 
+  function(material, salesorg){
+    
+    # Establish a connection to DuckDB
+    con <- dbConnect(duckdb(), dbdir = ":memory:")
+    
+    MATL <- c(MATN1(material))
+    SORG <- c(salesorg)
+    TABL <- FN_ISLS
+    
+    query <-   
+      glue_sql("
+    SELECT 
+      SALESORG,
+      MATERIAL,
+      CALMONTH,
+      sum(SLS_QT_SO + SLS_QT_FOC) as DEMND_QTY
+    FROM 
+      read_parquet({`TABL`})
+    WHERE
+      MATERIAL IN ({MATL*}) AND
+      SALESORG in ({SORG*}) AND
+      CALMONTH < date_trunc('month', current_date)
+    GROUP BY 
+      ALL
+    ORDER BY 
+      MATERIAL,
+      CALMONTH
+    ", .con = con
+      )
+    
+    # return data.table
+    dtTS <- 
+      dbGetQuery(con, query, n = Inf) %>%
+      setDT() %>%
+      .[, .(
+        unique_id = paste(SALESORG, MATERIAL, sep = "_"),
+        ds        = CALMONTH,
+        y         = DEMND_QTY
+      )
+      ]
+    
+    # Disconnect from DuckDB
+    dbDisconnect(con)
+    
+    return(dtTS)
   }
 
 fGet_PA_sales <- 
