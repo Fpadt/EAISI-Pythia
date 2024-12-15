13bee44c7e81915c981e417f4b888a15778b4274
Floris Padt <floris@fmpadt.nl>
2024-12-15 20:04:10 +0100

    cleaning en resturcture

--- library/pa_functions.R
+++ library/pa_functions.R
@@ -1,15 +1,17 @@
-fGet_tsMaterial <- 
-  function(x){
+fGet_dtMaterial <- 
+  function(material, salesorg){
     
     # Establish a connection to DuckDB
     con <- dbConnect(duckdb(), dbdir = ":memory:")
     
-    MATL <- c(MATN1(x))
+    MATL <- c(MATN1(material))
+    SORG <- c(salesorg)
     TABL <- FN_ISLS
     
     query <-   
       glue_sql("
     SELECT 
+      SALESORG,
       MATERIAL,
       CALMONTH,
       sum(SLS_QT_SO + SLS_QT_FOC) as DEMND_QTY
@@ -17,6 +19,7 @@ fGet_tsMaterial <-
       read_parquet({`TABL`})
     WHERE
       MATERIAL IN ({MATL*}) AND
+      SALESORG in ({SORG*}) AND
       CALMONTH < date_trunc('month', current_date)
     GROUP BY 
       ALL
@@ -29,7 +32,13 @@ fGet_tsMaterial <-
     # return data.table
     dtTS <- 
       dbGetQuery(con, query, n = Inf) %>%
-      setDT()
+      setDT() %>%
+      .[, .(
+        unique_id = paste(SALESORG, MATERIAL, sep = "_"),
+        ds        = CALMONTH,
+        y         = DEMND_QTY
+      )
+      ]
     
     # Disconnect from DuckDB
     dbDisconnect(con)
