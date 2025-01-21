09235aeff3b78f9e151d7dc7048f1ed22abbda8c
floris Padt <floris@fmpadt.nl>
2025-01-21 13:17:45 +0100

    read_config

--- library/init.R
+++ library/init.R
@@ -1,16 +1,86 @@
 library(fs)
 library(yaml)
+library(crayon)
 
-DTAP <- c("Development", "Test", "Acceptance", "Production")
-BSGP <- c("Bronze", "Silver", "Gold", "Platinum")
-AREA <- c("sales", "stock")
+DTAP <- c("Development", "Test"  , "Acceptance", "Production")
+BSGP <- c("Bronze"     , "Silver", "Gold"      , "Platinum")
+AREA <- c("sales"      , "stock" , "promotions", "master_data")
+
+.save_config_to_yaml <- function(
+    config_list, 
+    project_dir = ".") {
+  
+  # Normalize project directory
+  project_dir <- normalizePath(project_dir, mustWork = TRUE)
+  
+  # Define the path for the .config file
+  config_file <- file.path(project_dir, ".config.yaml")
+  
+  # Write the configuration data to the YAML file
+  write_yaml(config_list, config_file)
+  
+  message("Configuration saved to YAML file: ", config_file)
+}
+
+# .read_config_from_yaml <- function(
+#     project_dir = ".") {
+#   
+#   # Normalize project directory
+#   project_dir <- normalizePath(project_dir, mustWork = TRUE)
+#   
+#   # Define the path for the .config file
+#   config_file <- file.path(project_dir, ".config.yaml")
+#   
+#   # Check if the config file exists
+#   if (!file.exists(config_file)) {
+#     stop("Configuration file not found: ", config_file)
+#   }
+#   
+#   # Read the YAML file
+#   config_data <- read_yaml(config_file)
+#   
+#   return(config_data)
+# }
+
+.upsert_config_in_yaml <- 
+  function(
+    .key, 
+    .value, 
+    .project_dir = ".") {
+    
+  # Normalize project directory
+  project_dir <- normalizePath(.project_dir, mustWork = TRUE)
+  
+  # Define the path for the .config file
+  config_file <- file.path(project_dir, ".config.yaml")
+  
+  # Check if the config file exists
+  if (file.exists(config_file)) {
+    # Read the existing configuration
+    config_data <- read_yaml(config_file)
+  } else {
+    # Initialize an empty configuration if the file does not exist
+    config_data <- list()
+  }
+  
+  # Check if the key exists and update or insert
+  if (!.key %in% names(config_data)) {
+    message("Key '", .key, "' does not exist. Adding it to the configuration.")
+  }
+  config_data[[.key]] <- .value
+  
+  # Write the updated configuration back to the YAML file
+  write_yaml(config_data, config_file)
+  
+  message("Key '", .key, "' updated/added in the configuration file: ", config_file)
+}
 
 setup_project_structure <- function(
-    root_dir         = ".", 
-    dtap_levels      = DTAP,
-    bsgp_levels      = BSGP,
-    functional_areas = AREA,
-    project_dir      = "."
+    root_dir         = ".",  
+    dtap_levels      = DTAP,  # DTAP levels
+    bsgp_levels      = BSGP,  # staging levels
+    functional_areas = AREA,  # functional area's
+    project_dir      = "."    # to save .config.yaml
 ) {
   # Get OneDrive paths from environment variables
   onedrive_consumer <- path_abs(Sys.getenv("OneDriveConsumer"))
@@ -52,7 +122,7 @@ setup_project_structure <- function(
   }
   
   # Save the root_dir to the YAML file
-  save_config_to_yaml(
+  .save_config_to_yaml(
     config_list = list(root_dir = yaml_root_dir), 
     project_dir = project_dir
   )
@@ -62,90 +132,25 @@ setup_project_structure <- function(
 }
 
 
-save_config_to_yaml <- function(
-    config_list, 
-    project_dir = ".") {
-  
-  # Normalize project directory
-  project_dir <- normalizePath(project_dir, mustWork = TRUE)
-  
-  # Define the path for the .config file
-  config_file <- file.path(project_dir, ".config.yaml")
-  
-  # Write the configuration data to the YAML file
-  write_yaml(config_list, config_file)
-  
-  message("Configuration saved to YAML file: ", config_file)
-}
-
-read_config_from_yaml <- function(project_dir = ".") {
-  # Normalize project directory
-  project_dir <- normalizePath(project_dir, mustWork = TRUE)
-  
-  # Define the path for the .config file
-  config_file <- file.path(project_dir, ".config.yaml")
-  
-  # Check if the config file exists
-  if (!file.exists(config_file)) {
-    stop("Configuration file not found: ", config_file)
-  }
-  
-  # Read the YAML file
-  config_data <- read_yaml(config_file)
-  
-  return(config_data)
-}
-
-update_or_insert_config_in_yaml <- 
-  function(
-    .key, 
-    .value, 
-    .project_dir = ".") {
-    
-  # Normalize project directory
-  project_dir <- normalizePath(.project_dir, mustWork = TRUE)
-  
-  # Define the path for the .config file
-  config_file <- file.path(project_dir, ".config.yaml")
-  
-  # Check if the config file exists
-  if (file.exists(config_file)) {
-    # Read the existing configuration
-    config_data <- read_yaml(config_file)
-  } else {
-    # Initialize an empty configuration if the file does not exist
-    config_data <- list()
-  }
-  
-  # Check if the key exists and update or insert
-  if (!.key %in% names(config_data)) {
-    message("Key '", .key, "' does not exist. Adding it to the configuration.")
-  }
-  config_data[[.key]] <- .value
-  
-  # Write the updated configuration back to the YAML file
-  write_yaml(config_data, config_file)
-  
-  message("Key '", .key, "' updated/added in the configuration file: ", config_file)
-}
-
 # Get the current environment path
 set_current_environment <- function(
     .project_dir = ".", 
     .environment = "Production") {
   
   if(!.environment %in% DTAP){
+    message(green(paste0("Valid environments: ", paste(DTAP, collapse = ", "))))
     stop("Invalid environment: ", .environment)
   }
   
-  update_or_insert_config_in_yaml(
+  .upsert_config_in_yaml(
     .key         = "environment",
     .value       = .environment,
     .project_dir = .project_dir
   )
 }
 
-get_environment_path <- function(project_dir = ".") {
+get_environment_path <- function(
+    project_dir = ".") {
   
   # Normalize project directory
   project_dir <- path_abs(project_dir)
