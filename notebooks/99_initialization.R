caa527902a454397c8223d138b30e70e52184181
Floris Padt <floris@fmpadt.nl>
2025-02-08 17:28:03 +0100

    162 cross validation on 34, 14 different forecasts per article

--- notebooks/99_initialization.R
+++ notebooks/99_initialization.R
@@ -22,17 +22,46 @@ dtACC7 <- readRDS(
   file = file.path(PATH_SLV_SLS,"dtACC7.rds")
 )
 
+PATH_GLD_MD <- "C:\\PW\\OneDrive\\ET\\pythia\\data\\test\\Gold\\master_data"
+PATH_GLD_MD <- "C:\\Users\\flori\\OneDrive\\ET\\pythia\\data\\test\\Gold\\master_data"
+
+dtSCOPE <- fread(
+  file = file.path(PATH_GLD_MD, "SCP_162.csv")
+) %>% 
+  .[, MATERIAL:= pa_matn1_input(MATERIAL)]
+
 # filter the scope 
 # MATERIAL == MAT & 
 dtSLS <- 
-  dtACC7[SALESORG == SORG, 
+  dtSCOPE[SALESORG == SORG, 
          .(SALESORG, PLANT, MATERIAL)]                     %>%
   unique()                                                 %>%
   .[SLS, on = .(SALESORG, PLANT, MATERIAL), nomatch = 0]   %>%
   .[SALESORG == SORG & FTYPE == 4]     
 
+dtDUMMY <- 
+  dtSLS[CALMONTH %chin% c('202401', '202402')]  %>%
+  .[, CALMONTH:= sub("2024", "2025", CALMONTH)] %>%
+  .[, Q:= 0]
+
+
+  
+dtTST <-
+  rbind(dtSLS, dtDUMMY) %>%
+  .[, .(min = min(CALMONTH), max = max(CALMONTH)), 
+        by =.(SALESORG, PLANT, MATERIAL)] %>%
+  .[min == "202101" & max == "202502"]
+
+PATH_GLD_MD <- "C:\\PW\\OneDrive\\ET\\pythia\\data\\test\\Gold\\master_data"
+PATH_GLD_MD <- "C:\\Users\\flori\\OneDrive\\ET\\pythia\\data\\test\\Gold\\master_data"
+
+fwrite(
+  unique(dtTST[, .(SALESORG, PLANT, MATERIAL)]),
+  file = file.path(PATH_GLD_MD, "SCP_162.csv")
+) 
+
 tsSLS <- 
-  dtSLS %>%
+  rbind(dtSLS, dtDUMMY) %>%
   .[, .(MATERIAL, PLANT,  CALMONTH, ACT = Q)]              %>%
   .[, YM:= yearmonth(CALMONTH, format = "%Y%m")]   %>% 
   .[, CALMONTH:= NULL]                                     %>%  
