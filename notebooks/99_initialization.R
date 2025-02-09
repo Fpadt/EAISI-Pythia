35a6748e376c482651c98d24fecf566f38a69a38
Floris Padt <floris@fmpadt.nl>
2025-02-09 09:52:43 +0100

    restructure/rename and rescope -> 201 articles

--- notebooks/99_initialization.R
+++ notebooks/99_initialization.R
@@ -12,63 +12,67 @@ SLS <-                              #
     .scope_matl  = TRUE 
   ) 
 
-PATH_SLV_SLS <- pa_ds_stageing_path_get(
-  .staging = "silver", 
-  .functional_area = "sales", 
-  "rtp"
-)
+# PATH_SLV_SLS <- pa_ds_stageing_path_get(
+#   .staging = "silver", 
+#   .functional_area = "sales", 
+#   "rtp"
+# )
 
-dtACC7 <- readRDS(
-  file = file.path(PATH_SLV_SLS,"dtACC7.rds")
-)
+# dtACC7 <- readRDS(
+#   file = file.path(PATH_SLV_SLS,"dtACC7.rds")
+# )
 
 PATH_GLD_MD <- "C:\\PW\\OneDrive\\ET\\pythia\\data\\test\\Gold\\master_data"
 PATH_GLD_MD <- "C:\\Users\\flori\\OneDrive\\ET\\pythia\\data\\test\\Gold\\master_data"
 
 dtSCOPE <- fread(
-  file = file.path(PATH_GLD_MD, "SCP_162.csv")
+  file = file.path(PATH_GLD_MD, "SCOPE_FR30.csv")
 ) %>% 
   .[, MATERIAL:= pa_matn1_input(MATERIAL)]
 
 # filter the scope 
 # MATERIAL == MAT & 
 dtSLS <- 
-  dtSCOPE[SALESORG == SORG, 
-         .(SALESORG, PLANT, MATERIAL)]                     %>%
+  dtSCOPE[,.(SALESORG, PLANT, MATERIAL)]                   %>%
   unique()                                                 %>%
-  .[SLS, on = .(SALESORG, PLANT, MATERIAL), nomatch = 0]   %>%
-  .[SALESORG == SORG & FTYPE == 4]     
+  .[SLS, on = .(SALESORG, PLANT, MATERIAL), nomatch = 0]  
 
-dtDUMMY <- 
-  dtSLS[CALMONTH %chin% c('202401', '202402')]  %>%
-  .[, CALMONTH:= sub("2024", "2025", CALMONTH)] %>%
-  .[, Q:= 0]
+# dtDUMMY <- 
+#   dtSLS[CALMONTH %chin% c('202401', '202402')]  %>%
+#   .[, CALMONTH:= sub("2024", "2025", CALMONTH)] %>%
+#   .[, Q:= 0]
 
 
   
-dtTST <-
-  rbind(dtSLS, dtDUMMY) %>%
-  .[, .(min = min(CALMONTH), max = max(CALMONTH)), 
-        by =.(SALESORG, PLANT, MATERIAL)] %>%
-  .[min == "202101" & max == "202502"]
-
-PATH_GLD_MD <- "C:\\PW\\OneDrive\\ET\\pythia\\data\\test\\Gold\\master_data"
-PATH_GLD_MD <- "C:\\Users\\flori\\OneDrive\\ET\\pythia\\data\\test\\Gold\\master_data"
-
-fwrite(
-  unique(dtTST[, .(SALESORG, PLANT, MATERIAL)]),
-  file = file.path(PATH_GLD_MD, "SCP_162.csv")
-) 
+# dtTST <-
+#   rbind(dtSLS) %>%
+#   .[, .(min  = min(CALMONTH), max = max(CALMONTH),
+#         isna = sum(is.na(Q)), Q_0 = sum(Q==0)),
+#         by =.(SALESORG, PLANT, MATERIAL)]
+# 
+# # %>%
+# #   .[min == "202101" & max == "202502" & isna == 0 & Q_0 <= 3] 
+# 
+# PATH_GLD_MD <- "C:\\PW\\OneDrive\\ET\\pythia\\data\\test\\Gold\\master_data"
+# PATH_GLD_MD <- "C:\\Users\\flori\\OneDrive\\ET\\pythia\\data\\test\\Gold\\master_data"
+# 
+# fwrite(
+#   unique(dtTST[, .(SALESORG, PLANT, MATERIAL)]),
+#   file = file.path(PATH_GLD_MD, "SCP_156.csv")
+# ) 
 
 tsSLS <- 
-  rbind(dtSLS, dtDUMMY) %>%
-  .[, .(MATERIAL, PLANT,  CALMONTH, ACT = Q)]              %>%
-  .[, YM:= yearmonth(CALMONTH, format = "%Y%m")]   %>% 
-  .[, CALMONTH:= NULL]                                     %>%  
+  rbind(dtSLS) %>%
+  .[, .(SALESORG, PLANT, MATERIAL, CALMONTH, Q)]     %>%
+  .[, YM:= yearmonth(CALMONTH, format = "%Y%m")]     %>% 
+  .[, CALMONTH:= NULL]                               %>%  
   as_tsibble(
-    key   = c(MATERIAL, PLANT), 
+    key   = c(SALESORG, PLANT, MATERIAL), 
     index = YM
-  ) 
+  )                %>%
+  group_by_key()   %>%
+  fill_gaps(Q = 0) %>%
+  fill_gaps(.full = TRUE)
 
 months_diff <- 
   function(m1, m2) {
